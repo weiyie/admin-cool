bee67d503c5d422b4ce78f20310f8b4f4fd526bb
weiyie <912881342@qq.com>
2023-03-08 00:21:07 +0800

    任务列表

--- src/modules/share/views/task.vue
+++ src/modules/share/views/task.vue
@@ -3,8 +3,6 @@
 		<cl-row>
 			<!-- 刷新按钮 -->
 			<cl-refresh-btn />
-			<!-- 新增按钮 -->
-			<cl-add-btn />
 			<!-- 删除按钮 -->
 			<cl-multi-delete-btn />
 			<cl-flex1 />
@@ -26,22 +24,62 @@
 		</cl-row>
 
 		<!-- 新增、编辑 -->
-		<cl-upsert ref="Upsert" />
+		<cl-upsert ref="Upsert" :onInfo="onInfo" />
 	</cl-crud>
 </template>
 
 <script lang="ts" name="share-task" setup>
 import { useCrud, useTable, useUpsert } from "@cool-vue/crud";
+import { ref } from "vue";
 import { useCool } from "/@/cool";
 
 const { service } = useCool();
 
+const goodOptions = ref<any[]>([]);
+
+function getGoodList(val: string) {
+	service.share.info.list({ keyWord: val }).then((res) => {
+		goodOptions.value = res.map((item: any) => ({
+			label: item.goodName,
+			value: item.goodId
+		}));
+	});
+}
+
+function onInfo(data: Eps.ShareInfoEntity, { next }: any) {
+	goodOptions.value = [{ value: data.goodId, label: data.goodName }];
+	next(data);
+}
+
 // cl-upsert 配置
 const Upsert = useUpsert({
 	items: [
-		{ label: "商品ID", prop: "goodId", required: true, component: { name: "el-input" } },
-		{ label: "用户ID", prop: "userId", required: true, component: { name: "el-input" } },
-		{ label: "分组ID", prop: "groupId", required: true, component: { name: "el-input" } },
+		{
+			label: "商品",
+			prop: "goodId",
+			required: true,
+			component: {
+				name: "el-select",
+				options: goodOptions,
+
+				on: {
+					change: () => {
+						console.log(99999);
+					}
+				},
+				props: {
+					filterable: true,
+					remote: true,
+					"remote-method": getGoodList
+				}
+			}
+		},
+		{
+			label: "用户",
+			prop: "username",
+			component: { name: "el-input", props: { disabled: true } }
+		},
+		{ label: "分组", prop: "groupId", component: { name: "el-select" } },
 		{
 			label: "执行时间",
 			prop: "actionTime",
@@ -69,6 +107,7 @@ const Table = useTable({
 		},
 		{ label: "文案", prop: "text" },
 		{ label: "执行时间", prop: "actionTime" },
+		{ label: "重复", prop: "repeat" },
 		{ label: "用户", prop: "userName" },
 		{ label: "分组", prop: "groupName" },
 		{ type: "op", buttons: ["edit", "delete"] }
